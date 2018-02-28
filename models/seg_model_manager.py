0f5c65c2f412f498d485952c4b3de02c984e2b40
Donny You <youansheng@gmail.com>
2018-02-28 18:19:13 +0800

    seg models.

---
+++ models/seg_model_manager.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: Donny You(youansheng@gmail.com)
+# Select Seg Model for semantic segmentation.
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from models.seg.erf_net import ERFNet
+from utils.logger import Logger as Log
+
+
+class SegModelManager(object):
+
+    def __init__(self, configer):
+        self.configer = configer
+
+    def seg_net(self):
+        key = self.configer.get('network', 'model_name')
+        if key == 'erf_net':
+            return ERFNet(self.configer.get('network', 'out_channels'))
+        else:
+            Log.error('Model: {} not valid!'.format(key))
+            exit(0)
